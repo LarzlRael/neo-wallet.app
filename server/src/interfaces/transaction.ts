ed0cc41775f86578863dbd57ac7d3618a7d7c090
Reynaldo GT <larz.dev@gmail.com>
2022-08-28 20:40:46 -0400

    separating app in unique repository

--- server/src/interfaces/transaction.ts
+++
@@ -1,9 +0,0 @@
-export interface ITransaction {
-    originUser: string;
-    destinyUser:string;
-    targetUser: string;
-    amount: number;
-    
-    userOriginWallet: string,
-    userTargetWallet: string
-}
--- server/src/models/documents.ts
+++
@@ -1,3 +0,0 @@
-export const USERS = 'Users';
-export const WALLET = 'Wallet';
-export const TRANSACTION = 'transaction';
